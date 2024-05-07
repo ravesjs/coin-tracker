651450b61cf46ee93abe5d32c41f7b7e37e0dfc6
Rave JS <hswatter@mail.ru>
2024-05-07 14:02:09 +0300

    fix

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -2,7 +2,7 @@
 import React, { FC } from 'react'
 import { CryptoCoin } from '@/components/crypto-coin/CryptoCoin'
 import { useLocation } from 'react-router-dom'
-import { useActions } from '@/hooks/useActions'
+import { useFavorites } from '@/hooks/useFavorites'
 
 interface ICoins {
   id: number
@@ -10,12 +10,22 @@ interface ICoins {
 }
 
 const Favorites: FC<ICoins> = (coin) => {
-
+  const location = useLocation()
+  const { favorites } = useFavorites()
   const coins: ICoins[] = [
     { id: 1, name: 'Bitcoin' },
     { id: 2, name: 'Ethereum' },
     { id: 3, name: 'Cardano' },
   ]
+  if (location.pathname === '/favorites') {
+    return (
+      <>
+        {favorites.map((coin: any) => (
+          <CryptoCoin key={coin.id} id={coin.id} name={coin.name} />
+        ))}
+      </>
+    )
+  }
   return (
     <>
       {coins.map((coin) => (
--- src/services/providers/favorites/favorites.slice.js
+++ src/services/providers/favorites/favorites.slice.js
@@ -19,4 +19,5 @@ export const favoritesSlice = createSlice({
     },
   },
 })
+
 export const { actions, reducer } = favoritesSlice
