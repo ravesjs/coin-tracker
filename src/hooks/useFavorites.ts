d91d9f4233cf6ff6c1b60a026181b4efd6c82ed7
Rave JS <hswatter@mail.ru>
2024-05-26 07:29:18 +0300

    typed useSelectorHook

--- src/hooks/useFavorites.ts
+++ src/hooks/useFavorites.ts
@@ -1,8 +1,9 @@
-import { useSelector } from 'react-redux'
+import { useSelector, TypedUseSelectorHook } from 'react-redux'
 import {RootState} from '@/services/providers/store'
 
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 export const useFavorites = () => {
-  const favorites = useSelector((state: RootState) => state.favorites)
+  const favorites = useAppSelector((state: RootState) => state.favorites)
 
   return { favorites }
 }
