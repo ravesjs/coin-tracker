495f9d862f5fb9156c7a998629dd5d0eb9a99483
Rave JS <hswatter@mail.ru>
2024-05-26 07:30:29 +0300

    typed state

--- src/services/providers/favorites/favorites.slice.ts
+++ src/services/providers/favorites/favorites.slice.ts
@@ -1,16 +1,16 @@
+import { ICoin } from '@/components/Coin/Coin'
 import { createSlice } from '@reduxjs/toolkit'
-import { ICoin } from '@/components/crypto-coin/CryptoCoin'
 
-const initialState: ICoin[] = []
+const initialState: any[]  = []
 
 export const favoritesSlice = createSlice({
   name: 'favorites',
   initialState,
   reducers: {
     toggleFavorites: (state, { payload: crypto }) => {
-      const isExists = state.some((c) => c.id === crypto.id)
+      const isExists = state.some((c: ICoin) => c.id === crypto.id)
       if (isExists) {
-        const index = state.findIndex((c) => c.id === crypto.id)
+        const index = state.findIndex((c: ICoin) => c.id === crypto.id)
         if (index !== -1) {
           state.splice(index, 1)
         }
