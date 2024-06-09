e5c257aef5ec3a189068c0bebcf6d6eb5b81fe8c
Rave JS <hswatter@mail.ru>
2024-06-09 09:54:34 +0300

    router 6.4

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './services/providers/store'
-import App from './App'
+import { router } from '@/router'
+import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 )
