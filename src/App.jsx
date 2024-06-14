395d757d3b676cb06f52fbd78280640efea5f6b4
Rave JS <hswatter@mail.ru>
2024-06-14 14:19:06 +0300

    removed unnecessary index element in routes & layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import Home from '@/pages/HomePage'
 import Favorites from '@/pages/FavoritesPage'
-import Header from './components/Header/Header'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom'
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-      <Route path="/" element={<Header />}>
-        <Route index element={<Home />} />
-        <Route path="favorites" element={<Favorites />} />
-      </Route>
+    <>
+      <Route path="/" element={<Home />} />
+      <Route path="favorites" element={<Favorites />} />
+    </>
   )
 )
-const App = () => (
-  <RouterProvider router={router}/>
-)
+const App = () => <RouterProvider router={router} />
 
 export default App
