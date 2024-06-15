f4a24d60b06899423c59eae0b6b4635eac317ddc
Rave JS <hswatter@mail.ru>
2024-06-15 22:42:17 +0300

    added template type and moved it to config folder

--- config/paths.ts
+++ config/paths.ts
@@ -4,17 +4,14 @@ interface Paths {
   src: string
   build: string
   public: string
+  template: string
 }
 
 const paths: Paths = {
-  // Source files
   src: path.resolve(__dirname, '../src'),
-
-  // Production build files
   build: path.resolve(__dirname, '../dist'),
-
-  // Static files that get copied to build folder
   public: path.resolve(__dirname, '../public'),
+  template: path.resolve(__dirname, '../config')
 }
 
 export default paths
