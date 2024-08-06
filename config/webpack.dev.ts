09fc71156c1ca0f9bc4b11d4503ad320b6fa3cca
Rave JS <hswatter@mail.ru>
2024-08-06 16:33:34 +0300

    optimized webpack

--- config/webpack.dev.ts
+++ config/webpack.dev.ts
@@ -1,9 +1,9 @@
-import common from  './webpack.common'
+import common from './webpack.common'
 import { merge } from 'webpack-merge'
 import type { Configuration as DevServerConfiguration } from 'webpack-dev-server'
 import type { Configuration } from 'webpack'
 
-const dev: Configuration =  {
+const dev: Configuration = {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
@@ -16,22 +16,35 @@ const dev: Configuration =  {
   module: {
     rules: [
       {
-        test: /\.(sass|scss|css)$/i,
+        test: /\.module\.(sass|scss|css)$/i,
         use: [
           'style-loader',
           {
             loader: 'css-loader',
             options: {
               sourceMap: true,
-              importLoaders: 1,
-              modules: true,
+              importLoaders: 2,
+              modules: {
+                localIdentName: '[path][name]__[local]--[hash:base64:5]',
+              },
             },
           },
           { loader: 'postcss-loader', options: { sourceMap: true } },
           { loader: 'sass-loader', options: { sourceMap: true } },
         ],
       },
+      {
+        test: /\.(sass|scss|css)$/i,
+        exclude: /\.module\.(sass|scss|css)$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
     ],
   },
 }
+
 export default merge(common, dev)
